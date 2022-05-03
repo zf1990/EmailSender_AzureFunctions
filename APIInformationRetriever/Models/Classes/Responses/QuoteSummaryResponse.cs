c5c31b871b9f2b7872aa6ae8cda7e0e4affa99f6
Zhou Fang <fang1534@stthomas.edu>
2022-05-02 21:11:55 -0500

    -Created FileWRiter project. -Added project reference

---
+++ APIInformationRetriever/Models/Classes/Responses/QuoteSummaryResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIInformationRetriever.Models.Classes.Responses
+{
+    public class QuoteSummaryResponse : IResponse
+    {
+    }
+}
---
+++ FileWritter/Writer.cs
@@ -0,0 +1,22 @@
+﻿using APIInformationRetriever.Models.Classes.Responses;
+
+namespace FileWritter
+{
+    public class Writer
+    {
+        public Writer(IResponse Response)
+        {
+            switch(Response)
+            {
+                case QuoteResponse res:
+                    break;
+                case SparkResponse res:
+                    break;
+                case QuoteSummaryResponse res:
+                    break;
+                default:
+                    throw new ArgumentException("This response is not supported at the moment");
+            }
+        }
+    }
+}
